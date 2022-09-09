769fcda2374bf83a0b5284754e9fc2b02f378c29
olgan95 <olgan9595@gmail.com>
2022-09-09 15:08:36 +0200

    adding index.html to package.json

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, HashRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
           <App />
       </HashRouter>
   </React.StrictMode>
